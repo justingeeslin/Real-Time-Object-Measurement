b0b8ba96ae391b7cbc8ac613ec99631af7afab1f
Justin Geeslin <justin@geesl.in>
2026-01-29 09:30:58 -0600

    Paramaterizing with different reference size objects

--- tests/TestObjectMeasurement.py
+++ tests/TestObjectMeasurement.py
@@ -3,18 +3,20 @@ import pytest
 
 from ObjectMeasurement import ObjectMeasurer
 
+A4_MM = (210.0, 297.0)
+LETTER_MM = (215.9, 279.4)  # 8.5in x 11in
 
 @pytest.mark.parametrize(
-    "image_path, expected_count, expected_w_cm, expected_h_cm, tol_cm",
+    "image_path, reference_size_mm, expected_count, expected_w_cm, expected_h_cm, tol_cm",
     [
-        ("../1.jpg", 2, 9.0, 5.0, 1.0),  # adjust tol if your lighting/edges vary
+        ("../test-images/one/one.jpg", A4_MM, 2, 9.0, 5.0, 1.0),  # adjust tol if your lighting/edges vary
     ],
 )
-def test_1jpg_two_objects_about_9x5(image_path, expected_count, expected_w_cm, expected_h_cm, tol_cm):
+def test_1jpg_two_objects_about_9x5(image_path, reference_size_mm, expected_count, expected_w_cm, expected_h_cm, tol_cm):
     img = cv2.imread(image_path)
     assert img is not None, f"Could not load image at path: {image_path}"
 
-    measurer = ObjectMeasurer(scale=3)
+    measurer = ObjectMeasurer(scale=3, reference_size_mm=reference_size_mm)
 
     measurements = measurer.measure(img)
 
